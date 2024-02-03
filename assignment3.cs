72ed290f91094fd9ca8bfa53ca93be71504373d4
swaroop1203 <158731103+swaroop1203@users.noreply.github.com>
2024-02-03 11:45:09 +0000

    added assignment 3

---
+++ assignment3.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Person
+{
+    static void Main()
+    {
+        string name = "Sulav";
+        int age = 22;
+        string hometown = "Sindhuli";
+        string fav_color = "Green";
+        bool likes_rock_music = true;
+
+        Console.WriteLine("name: " + name);
+        Console.WriteLine("age: " + age);
+        Console.WriteLine("hometown: " + hometown);
+        Console.WriteLine("favorite color: " + fav_color);
+        Console.WriteLine("likes rock music: " + likes_rock_music);
+    }
+}
